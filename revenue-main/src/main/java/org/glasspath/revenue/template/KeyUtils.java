e57961fe708277fbea1d8d417d81161757f66a60
remcopoelstra <remco@poelstrabesturingen.nl>
2023-01-23 19:51:27 +0100

    Added template keys and descriptions

--- revenue-main/src/main/java/org/glasspath/revenue/template/KeyUtils.java
+++ revenue-main/src/main/java/org/glasspath/revenue/template/KeyUtils.java
@@ -27,21 +27,86 @@ import java.util.ResourceBundle;
 @SuppressWarnings("nls")
 public class KeyUtils {
 
-	public static final ResourceBundle BUNDLE = ResourceBundle.getBundle("org.glasspath.revenue.template.keys");
+	public static final ResourceBundle KEYS = ResourceBundle.getBundle("org.glasspath.revenue.template.keys");
+	public static final ResourceBundle DESCRIPTIONS = ResourceBundle.getBundle("org.glasspath.revenue.template.descriptions");
 
-	public static final Key AMOUNT = new Key("amount");
-	public static final Key CLIENT = new Key("client");
-	public static final Key COMMENTS = new Key("comments");
-	public static final Key DATE = new Key("date");
-	public static final Key DATE_FROM = new Key("date_from");
-	public static final Key DATE_TO = new Key("date_to");
-	public static final Key HOUR = new Key("hour");
-	public static final Key INVOICE = new Key("invoice");
-	public static final Key NUMBER = new Key("number");
-	public static final Key PROJECT = new Key("project");
-	public static final Key RATE = new Key("rate");
-	public static final Key TIME_FROM = new Key("time_from");
-	public static final Key TIME_TO = new Key("time_to");
+	public static final Key ADDRESS = new Key("Address");
+	public static final Key ADDRESS_1 = new Key("Address1");
+	public static final Key ADDRESS_2 = new Key("Address2");
+	public static final Key AMOUNT = new Key("Amount");
+
+	public static final Key BANK_ACCOUNT = new Key("BankAccount");
+	public static final Key BUSINESS_NUMBER = new Key("BusinessNumber");
+
+	public static final Key CALL = new Key("Call");
+	public static final Key CALLS = new Key("Calls");
+	public static final Key CITY = new Key("City");
+	public static final Key CLIENT = new Key("Client");
+	public static final Key COMMENTS = new Key("Comments");
+	public static final Key COMPANY = new Key("Company");
+	public static final Key COUNTRY = new Key("Country");
+
+	public static final Key DATE = new Key("Date");
+	public static final Key DATE_FROM = new Key("DateFrom");
+	public static final Key DATE_TO = new Key("DateTo");
+	public static final Key DATE_PAID = new Key("DatePaid");
+	public static final Key DESCRIPTION = new Key("Description");
+	public static final Key DUE_DATE = new Key("DueDate");
+	public static final Key DURATION = new Key("Duration");
+
+	public static final Key EMAIL = new Key("Email");
+	public static final Key EXPENSE = new Key("Expense");
+	public static final Key EXPENSES = new Key("Expenses");
+
+	public static final Key FAX = new Key("Fax");
+	public static final Key FROM = new Key("From");
+
+	public static final Key HOUR = new Key("Hour");
+	public static final Key HOURS = new Key("Hours");
+
+	public static final Key INVOICE = new Key("Invoice");
+	public static final Key INVOICES = new Key("Invoices");
+	public static final Key INVOICE_LINE = new Key("InvoiceLine");
+
+	public static final Key MILEAGE = new Key("Mileage");
+	public static final Key MILEAGES = new Key("Mileages");
+
+	public static final Key NAME = new Key("Name");
+	public static final Key NUMBER = new Key("Number");
+
+	public static final Key PERIOD = new Key("Period");
+	public static final Key PO_BOX = new Key("Po_box");
+	public static final Key POSTAL_CODE = new Key("PostalCode");
+	public static final Key PRICE = new Key("Price");
+	public static final Key PROJECT = new Key("Project");
+
+	public static final Key QUANTITY = new Key("Quantity");
+
+	public static final Key RATE = new Key("Rate");
+	public static final Key REFERENCE = new Key("Reference");
+	public static final Key ROUTE = new Key("Route");
+
+	public static final Key STATE = new Key("State");
+	public static final Key STATUS = new Key("Status");
+	public static final Key STREET = new Key("Street");
+	public static final Key SUPPLIER = new Key("Supplier");
+
+	public static final Key TELEPHONE = new Key("Telephone");
+	public static final Key TIME = new Key("Time");
+	public static final Key TIME_FROM = new Key("TimeFrom");
+	public static final Key TIME_TO = new Key("TimeTo");
+	public static final Key TOTAL = new Key("Total");
+	public static final Key TOTAL_EXCL = new Key("TotalExcl");
+	public static final Key TOTAL_INCL = new Key("TotalIncl");
+
+	public static final Key VAT = new Key("Vat");
+	public static final Key VAT_NUMBER = new Key("VatNumber");
+	public static final Key VAT_RATE = new Key("VatRate");
+
+	public static final Key WEBSITE = new Key("Website");
+	public static final Key WEEK_NUMBER = new Key("WeekNumber");
+
+	public static final Key YEAR = new Key("Year");
 
 	protected KeyUtils() {
 
@@ -51,7 +116,7 @@ public class KeyUtils {
 
 		if (key.value == null) {
 			try {
-				key.value = BUNDLE.getString(key.key);
+				key.value = KEYS.getString(key.key);
 			} catch (Exception e) {
 				key.value = key.key;
 			}
@@ -61,16 +126,54 @@ public class KeyUtils {
 
 	}
 
+	public static String description(Key key) {
+
+		if (key.description == null) {
+			try {
+				key.description = DESCRIPTIONS.getString(key.key);
+			} catch (Exception e) {
+				key.description = key.key;
+			}
+		}
+
+		return key.description;
+
+	}
+
 	public static String clientKey(Key key) {
 		return key(CLIENT, key);
+	}
+
+	public static String clientAddress1Key(Key key) {
+		return key(CLIENT, ADDRESS_1, key);
+	}
+
+	public static String clientAddress2Key(Key key) {
+		return key(CLIENT, ADDRESS_2, key);
 	}
 
 	public static String hourKey(Key key) {
 		return key(HOUR, key);
 	}
 
+	public static String mileageKey(Key key) {
+		return key(MILEAGE, key);
+	}
+
+	public static String callKey(Key key) {
+		return key(CALL, key);
+	}
+
 	public static String invoiceKey(Key key) {
 		return key(INVOICE, key);
+	}
+
+	public static String invoiceLineKey(Key key) {
+		return key(INVOICE_LINE, key);
+	}
+
+	public static String vatRateKey(Key key) {
+		return key(VAT_RATE, key);
 	}
 
 	public static String tKey(String key) {
@@ -85,10 +188,19 @@ public class KeyUtils {
 		return key(key1) + separator + key(key2);
 	}
 
+	public static String key(Key key1, Key key2, Key key3) {
+		return key(key1, key2, key3, ".");
+	}
+
+	public static String key(Key key1, Key key2, Key key3, String separator) {
+		return key(key1) + separator + key(key2) + separator + key(key3);
+	}
+
 	public static class Key {
 
 		private final String key;
 		private String value = null;
+		private String description = null;
 
 		protected Key(String key) {
 			this.key = key;
